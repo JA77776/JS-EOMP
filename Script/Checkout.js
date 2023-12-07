0e60cd2f3e70999c9e611655e9223f60dd2178aa
JA77776 <jaydonnhendricks09@gmail.com>
2023-12-07 16:28:14 +0200

    products

--- Script/Checkout.js
+++ Script/Checkout.js
@@ -2,3 +2,25 @@
 
 document.querySelector('#currentYear').textContent =
 new Date().getUTCFullYear()
+
+
+// checkout
+
+let checkout = JSON.parse(localStorage.getItem('cart'))
+console.log(checkout)
+
+
+let displaydProducts = document.querySelector('display-products')
+
+function toDisplayMyProducts() {
+    displaydProducts.innerHTML = '';
+    disp.foreach( item => {
+        results.innerHTML +=
+        `<tr>
+        <th scope="col"></th>
+        <td>${item.name}</td>
+        <td><input id="input" placeholder="number" type="number" ></td>
+        <td>${item.price}</td>
+      </tr>`
+    } )
+}
